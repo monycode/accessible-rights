8095afac72c09e28eb25d50ed3105f8c2d0cabd7
Monica Rukeba <monicarukeba@gmail.com>
2022-06-09 20:24:11 -0600

    adding fields to devise edit portion of user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_024355) do
+ActiveRecord::Schema.define(version: 2022_06_10_013315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2022_06_03_024355) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.boolean "verified"
+    t.string "job_title"
+    t.string "location"
+    t.string "education"
+    t.string "law_firm"
+    t.text "bio"
+    t.string "twitter_handle"
+    t.string "linkedin_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
