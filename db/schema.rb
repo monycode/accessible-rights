9ebf928042297bb2f9a1a5f91a037162ef07a14b
Monica Rukeba <monicarukeba@gmail.com>
2022-06-11 13:07:34 -0600

    adding question and answer models with references and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_024355) do
+ActiveRecord::Schema.define(version: 2022_06_11_182445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "content"
+    t.bigint "question_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
 
   create_table "forums", force: :cascade do |t|
     t.string "name"
@@ -32,6 +42,17 @@ ActiveRecord::Schema.define(version: 2022_06_03_024355) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.bigint "forum_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["forum_id"], name: "index_questions_on_forum_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,10 +63,21 @@ ActiveRecord::Schema.define(version: 2022_06_03_024355) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.boolean "verified"
+    t.string "job_title"
+    t.string "location"
+    t.string "education"
+    t.string "law_firm"
+    t.text "bio"
+    t.string "twitter_handle"
+    t.string "linkedin_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
   add_foreign_key "messages", "forums"
   add_foreign_key "messages", "users"
+  add_foreign_key "questions", "forums"
+  add_foreign_key "questions", "users"
 end
