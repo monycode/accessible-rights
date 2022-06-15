24287c470c2c1e1aa733e248e30c566b1dde696a
Jorge Alejandro Avelar Gonzalez <avelargonzalez@gmail.com>
2022-06-14 21:24:46 -0500

    changes to routes

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,6 +1,7 @@
 class ForumsController < ApplicationController
+  skip_before_action :authenticate_user!
   def show
-    @forum = Forum.find(params[:id])
+    @forum = Forum.find_by(name: params[:name])
     @question = Question.new
     #pendiente - mostrar preguntas ya creadas - otra variable
   end
