629f7c8da8fa153e4e92bb5e0d308a38f8882a64
Sebastian Valencia <asvalenciareynoso@gmail.com>
2022-06-25 09:19:44 -0500

    not final

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
 class ProfilesController < ApplicationController
+  skip_before_action :authenticate_user!
   def show
-
+    @user = User.find(params[:id])
+    #@question = Question.new
+    #pendiente - mostrar preguntas ya creadas - otra variable
   end
 end
